bfa4ab4eeaac34a26aed59140ceccf06d9b0ddf0
Hannes Roegiers <hannes.roegiers@student.hogent.be>
2022-10-30 16:24:37 +0100

    google search de bedrijven

--- websites/websites.py
+++ websites/websites.py
@@ -1,19 +1,25 @@
 import os
 import pandas as pd
 import requests
+from googlesearch import search
 from bs4 import BeautifulSoup
 
 
 df = pd.read_excel("websites\kmo's_Vlaanderen_2021.xlsx",usecols="B,C,D,E,L",sheet_name="Lijst")
+
+
 website=0
 geen_website=0
 counter=0
 max=10
 for index,row in df.iterrows():
-    if isinstance(row[4],str):
-        website+=1 
-    
-print(geen_website)
-print(website)
+    if counter <= max:
+        if isinstance(row[4],float):
+            for j in search(row[0],num_results=2):
+                print(j)
+            print(f'{counter+1}\n')
+            counter+=1
+
+
 
         
