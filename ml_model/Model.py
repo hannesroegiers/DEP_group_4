dc827c63376136134622d09448d7a9cc3f1f4447
Philippe S <philippe.strijk@student.hogent.be>
2022-11-06 15:28:48 +0100

    Grote lijnen no code

---
+++ ml_model/Model.py
@@ -0,0 +1,17 @@
+
+
+import numpy as np
+import pandas as pd
+import tensorflow
+from tensorflow import keras
+import matplotlib as mpl
+
+# STAP 1: data inladen
+
+# STAP 2: data visualiseren, statistieken bekijken, inzichten ophalen, zoeken naar correlaties
+
+# STAP 3: data voorbereiden pipeline, redundante data verwijderen, test set opstellen
+#   labels en data
+#   pipeline (missing values, scaling, onehotencoders, etc...)
+
+# STAP 4: selecteer, train en evalueer (grid search cross-validation (CV) of randomnized search CV) het model
