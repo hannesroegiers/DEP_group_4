78959a49fe3b64bc34ea7230fef3e743a810c7fd
Wout Boeykens <wout.boeykens@student.hogent.be>
2022-11-19 11:55:31 +0100

    ""

--- score berekenen.sql
+++ score berekenen.sql
@@ -2,7 +2,7 @@ select kmo.ondernemingsnummer, bedrijfsnaam, ts_rank_cd(ts_tekst, query) as rank
 	(ts_rank(ts_tekst, query1) + ts_rank(ts_tekst, query2) + ts_rank(ts_tekst, query3)) / 3 as avg,
 	ts_rank(ts_tekst, query1) as rank1, 
 	ts_rank(ts_tekst, query2) as rank2, 
-	ts_rank(ts_tekst, query3) as rank3,
+	ts_rank(ts_tekst, query3) as rank3
 	--ts_headline('dutch', tekst, query, 'MaxWords=10,MinWords=1,StartSel = [,StopSel = ],MaxFragments=10,FragmentDelimiter=...')
 from jaarverslag jv join kmo on jv.ondernemingsnummer = kmo.ondernemingsnummer, 
 	to_tsquery('dutch','(leven & land) | biodiversiteit | (leven & water)') query,
