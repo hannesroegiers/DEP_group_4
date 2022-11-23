7b488fc8003d4295dd7c878f1a16538c629019e4
Philippe S <philippe.strijk@student.hogent.be>
2022-11-23 16:04:46 +0100

    Oprichtingsdata in database + Excel sheet met alle oprichtingsdata

--- ml_data.py
+++ ml_data.py
@@ -10,6 +10,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import time
+import numpy as np
 
 # initialization of PostgreSQL stuff
 pg_engine = create_engine('postgresql://postgres:loldab123@vichogent.be:40031/durabilitysme')
@@ -22,7 +23,7 @@ pg_Base.metadata.reflect(pg_engine)   # get metadata from database
 Session = sessionmaker(bind=pg_engine)
 pg_session = Session()
 
-def machinelearningdata_opvullen():
+""" def machinelearningdata_opvullen():
     class PG_SME(pg_Base):  # each table is a subclass from the Base class
         __table__ = pg_Base.metadata.tables['machinelearningData']
 
@@ -36,44 +37,65 @@ def machinelearningdata_opvullen():
         pg_machinelearningData = PG_SME(ondernemingsnummer=int(rij[7].replace(' ','')), jaar=2021, personeelsledenAantal= rij[4], omzet= omzet)
         pg_session.add(pg_machinelearningData)
     
-    pg_session.commit()
+    pg_session.commit() """
 
-def scrape_oprichtingsjaar(ondernemingsnummer):
-    URL = "https://kbopub.economie.fgov.be/kbopub/zoeknummerform.html?lang=nl&nummer="+str(ondernemingsnummer)+"&actionLu=Recherche"
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    result = soup.find("span", {"class":"upd"}).text #output : 'Sinds 9 augustus 1960'
-    result = result[::-1] #output : '0691 sutsugua sdniS'
-    result = result[:4] #output : '0691'
-    result = int(result[::-1]) #output : '1960'
-    return(result)
+
+def geef_oprichtingsjaar(result, ondernemingsnummer):
+
+
+    end = result.loc[result['EnterpriseNumber'] == ondernemingsnummer]['StartDate'].values[0]
+
+    jaar = end[6:]
+
+    return(jaar)
+
+
+
 
 def opvullen_alle_oprichtingsjaren():
     class PG_SME(pg_Base):  # each table is a subclass from the Base class
         __table__ = pg_Base.metadata.tables['machinelearningData']
 
     xl_file = pd.read_excel("DEP_group_4/websites/kmo's_Vlaanderen_2021.xlsx", sheet_name= "Lijst")
+    xl_file = xl_file.sort_values('Ondernemingsnummer')
+    
+    all_enterprises = pd.read_csv("DEP_group_4/websites/enterprise.csv")
+    start_dates = all_enterprises.loc[:,["EnterpriseNumber","StartDate"]]
+    ondernemingsnummers = pd.read_excel("DEP_group_4/websites/kmo's_Vlaanderen_2021.xlsx", sheet_name= "Lijst", usecols=[7])
+    
+    start_dates['EnterpriseNumber'] = start_dates['EnterpriseNumber'].astype('str').str.replace(r".", r"", regex=False)
+    ondernemingsnummers['Ondernemingsnummer'] = ondernemingsnummers['Ondernemingsnummer'].astype('str').str.replace(r" ", r"",regex=False)
+    
+    result = start_dates[start_dates['EnterpriseNumber'].isin(ondernemingsnummers['Ondernemingsnummer'].to_list())]
 
     for rij in xl_file.values:
         
         ondernemingsnummer = rij[7].replace(' ','')
-        ondernemingsnummer = ondernemingsnummer[1:]
+        
         try:
-            oprichtingsjaar = scrape_oprichtingsjaar(ondernemingsnummer)
+            oprichtingsjaar = geef_oprichtingsjaar(result, ondernemingsnummer)
+            ondernemingsnummer = ondernemingsnummer[1:]
             print("Ondernemingsnummer: " + ondernemingsnummer + "  Oprichtingsjaar: " + str(oprichtingsjaar))
-            pg_machinelearningData = PG_SME(oprichtingsjaar=oprichtingsjaar)
-            pg_session.add(pg_machinelearningData)
+            print(rij[1])
+            omzet = rij[5] if rij[5] != 'n.b.' else 0
+            pg_session.execute(
+                    update(PG_SME)
+                    .where(PG_SME.ondernemingsnummer == ondernemingsnummer)
+                    .values(oprichtingsjaar=oprichtingsjaar)
+                )           
         except:
             pass
         
-    pg_session.commit()
+    pg_session.commit() 
 
 try:
-
     opvullen_alle_oprichtingsjaren()
-#machinelearningdata_opvullen()
+    #machinelearningdata_opvullen()
 
 finally:    
     pg_session.close()
     pg_conn.close()
     print("Connections closed")
+
+
+    
